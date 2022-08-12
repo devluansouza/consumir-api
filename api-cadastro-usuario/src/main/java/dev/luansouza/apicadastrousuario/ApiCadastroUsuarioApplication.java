c6dce15b2fe6b6138891d334d9c1ea9aad4054b3
Luan Souza <luan.souza2014@gmail.com>
2022-08-11 23:02:55 -0300

    Adiciona o projeto api-cadastro-usuario ao repositório

---
+++ api-cadastro-usuario/src/main/java/dev/luansouza/apicadastrousuario/ApiCadastroUsuarioApplication.java
@@ -0,0 +1,13 @@
+package dev.luansouza.apicadastrousuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCadastroUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCadastroUsuarioApplication.class, args);
+	}
+
+}
---
+++ api-cadastro-usuario/src/test/java/dev/luansouza/apicadastrousuario/ApiCadastroUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.luansouza.apicadastrousuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCadastroUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
